84d215ada56efb9925c5f4195eab79d05dc18c35
alterasc <alterasc@protonmail.com>
2023-12-13 00:57:50 +0100

    add very lazy UI

--- Main.cs
+++ Main.cs
@@ -4,11 +4,14 @@ using Kingmaker;
 using Kingmaker.Code.UI.MVVM.VM.ServiceWindows;
 using Kingmaker.Settings.Entities;
 using System;
+using UnityEngine;
 using UnityModManagerNet;
 
 namespace EnhancedControls;
 
+#if DEBUG
 [EnableReloading]
+#endif
 static class Main
 {
     internal static Harmony HarmonyInstance;
@@ -37,9 +40,41 @@ static class Main
         {
             HarmonyInstance.CreateClassProcessor(typeof(CollectAllAndClose.BindPatches)).Patch();
         }
-
+        modEntry.OnGUI = OnGUI;
+        modEntry.OnSaveGUI = OnSaveGUI;
         Settings.Save(modEntry);
         return true;
+    }
+
+    static void OnGUI(UnityModManager.ModEntry modEntry)
+    {
+        GUIStyle redLabelStyle = new(GUI.skin.label);
+        redLabelStyle.normal.textColor = Color.red;
+        GUILayout.Label("All changes are applied ONLY AFTER RESTART", redLabelStyle);
+        GUILayout.Space(5);
+        GUILayout.Label("To change - use keycodes taken from https://docs.unity3d.com/ScriptReference/KeyCode.html");
+        GUILayout.Label("To remove - leave field empty.");
+        GUILayout.Label("Support for entering keys by pressing them will come later");
+
+        GUILayout.Space(20);
+        var labelWidth = GUILayout.Width(GUI.skin.label.CalcSize(new GUIContent("Lore (Imperium)                ")).x);
+        // Iterate over fields
+        foreach (var field in Settings.GetType().GetFields())
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(field.Name, labelWidth);
+            field.SetValue(Settings, GUILayout.TextField(field.GetValue(Settings).ToString(), GUILayout.Width(150)));
+            GUILayout.EndHorizontal();
+        }
+
+        if (GUI.changed)
+        {
+            Settings.Save(modEntry);
+        }
+    }
+    static void OnSaveGUI(UnityModManager.ModEntry modEntry)
+    {
+        Settings.Save(modEntry);
     }
 
     public static bool TryParseKeyBinding(string keyBinding, out KeyBindingData? keyBindingData)
